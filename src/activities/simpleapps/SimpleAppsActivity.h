46674518c149c28483b553e5815b92405be2e881
Mary Luongo <mary.luongo@jhuapl.edu>
2026-05-01 14:12:07 -0400

    json change

--- src/activities/simpleapps/SimpleAppsActivity.h
+++ src/activities/simpleapps/SimpleAppsActivity.h
@@ -1,40 +1,28 @@
 #pragma once
-#include <cstdint>
-#include <string>
+
+#include "../Activity.h"
+#include "util/ButtonNavigator.h"
+#include <ArduinoJson.hpp>
 #include <vector>
-#include <ArduinoJson.hpp>   // REQUIRED
+#include <string>
 
-struct SimpleAppEntry{std::string name;std::string path;std::string type;};
-struct SimpleDrawResult{int itemIndex=0;bool reversed=false;};
-enum class SimpleAppsMode:uint8_t{AppList,AppMenu,Result};
+class SimpleAppsActivity final : public Activity {
+  ButtonNavigator nav;
 
-class SimpleAppsActivity final:public Activity{
-  ButtonNavigator buttonNavigator;
-  SimpleAppsMode mode=SimpleAppsMode::AppList;
-  std::vector<SimpleAppEntry> apps;
-  ArduinoJson::DynamicJsonDocument currentApp{8192};
-  int selectedApp=0,selectedAction=0,selectedResult=0;
-  std::vector<SimpleDrawResult> drawResults;
-  uint32_t seed=1;
-  uint32_t nextRandom();
-  int randomIndex(int maxExclusive);
-  bool randomBool();
-  void loadApps();
-  bool loadCurrentApp();
-  void runSelectedAction();
-  void renderAppList();
-  void renderAppMenu();
-  void renderResult();
-  void renderCoinFlipResult();
-  void renderRandomAnswerResult();
-  void renderRandomDrawResult();
-  void renderDailyComboResult();
-  void drawCardBox(int x,int y,int w,int h,const char* symbol,bool reversed);
-  void drawWrappedText(int x,int& y,int maxWidth,const char* text,int maxLines);
+  std::vector<std::string> apps;
+  int selected = 0;
+
+  ArduinoJson::JsonDocument currentApp;
+
  public:
-  explicit SimpleAppsActivity(GfxRenderer& renderer,MappedInputManager& mappedInput)
-    :Activity("Simple Apps",renderer,mappedInput){}
+  SimpleAppsActivity(GfxRenderer& r, MappedInputManager& i)
+      : Activity("Apps", r, i) {}
+
   void onEnter() override;
   void loop() override;
   void render(RenderLock&&) override;
+
+ private:
+  void loadApps();
+  bool loadApp(const std::string& path);
 };
