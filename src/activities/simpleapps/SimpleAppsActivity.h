d9b020f3ca0b3a1ffcae9a7f05b7ab19d7460c33
Mary Luongo <mary.luongo@jhuapl.edu>
2026-05-01 08:36:55 -0400

    changes to apps and rename

--- src/activities/simpleapps/SimpleAppsActivity.h
+++ src/activities/simpleapps/SimpleAppsActivity.h
@@ -1,69 +1,41 @@
 #pragma once
-
 #include <ArduinoJson.h>
-
 #include <cstdint>
 #include <string>
 #include <vector>
-
 #include "../Activity.h"
 #include "util/ButtonNavigator.h"
 
-struct SimpleAppEntry {
-  std::string name;
-  std::string path;
-  std::string type;
-};
+struct SimpleAppEntry{std::string name;std::string path;std::string type;};
+struct SimpleDrawResult{int itemIndex=0;bool reversed=false;};
+enum class SimpleAppsMode:uint8_t{AppList,AppMenu,Result};
 
-struct SimpleDrawResult {
-  int itemIndex = 0;
-  bool reversed = false;
-};
-
-enum class SimpleAppsMode : uint8_t {
-  AppList,
-  AppMenu,
-  Result,
-};
-
-class SimpleAppsActivity final : public Activity {
+class SimpleAppsActivity final:public Activity{
   ButtonNavigator buttonNavigator;
-
-  SimpleAppsMode mode = SimpleAppsMode::AppList;
-
+  SimpleAppsMode mode=SimpleAppsMode::AppList;
   std::vector<SimpleAppEntry> apps;
   JsonDocument currentApp;
-
-  int selectedApp = 0;
-  int selectedAction = 0;
-  int selectedResult = 0;
-
+  int selectedApp=0,selectedAction=0,selectedResult=0;
   std::vector<SimpleDrawResult> drawResults;
-  uint32_t seed = 1;
-
+  uint32_t seed=1;
   uint32_t nextRandom();
   int randomIndex(int maxExclusive);
   bool randomBool();
-
   void loadApps();
   bool loadCurrentApp();
   void runSelectedAction();
-
   void renderAppList();
   void renderAppMenu();
   void renderResult();
-
   void renderCoinFlipResult();
   void renderRandomAnswerResult();
   void renderRandomDrawResult();
-
-  void drawCardBox(int x, int y, int w, int h, const char* symbol, bool reversed);
-  void drawWrappedText(int x, int& y, int maxWidth, const char* text, int maxLines);
-
+  void renderDailyComboResult();
+  void drawCardBox(int x,int y,int w,int h,const char* symbol,bool reversed);
+  void drawWrappedText(int x,int& y,int maxWidth,const char* text,int maxLines);
  public:
-  explicit SimpleAppsActivity(GfxRenderer& renderer, MappedInputManager& mappedInput)
-      : Activity("Simple Apps", renderer, mappedInput) {}
-
+  explicit SimpleAppsActivity(GfxRenderer& renderer,MappedInputManager& mappedInput)
+    :Activity("Simple Apps",renderer,mappedInput){}
   void onEnter() override;
   void loop() override;
   void render(RenderLock&&) override;
